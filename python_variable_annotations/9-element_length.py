6c00d01ed8aa91f4568abbb20361952ab1e19bc0
Adib-96 <adibbh1996@gmail.com>
2024-01-01 17:49:46 +0100

    Standard “duck types”

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+Returns a list of tuples containing elements and their lengths
+"""
+
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """Returns a list of tuples containing elements and their lengths.
+
+    Args:
+        lst (List[str]): The input list of strings.
+
+    Returns:
+        A list of tuples.
+    """
+    return [(i, len(i)) for i in lst]
+
+
+print(element_length(["af", "bqq", "cqsd"]))
