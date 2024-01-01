9ab4a704c71f7ad0a8f29ca6d33c5480eb799e8d
Adib-96 <adibbh1996@gmail.com>
2024-01-01 16:11:11 +0100

    Basic annotations-add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+function add that takes
+a float a and a float b as arguments and
+returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """function mnemonic SUM"""
+    return a + b
+
+
+
+print(add(1.11, 2.22) == 1.11 + 2.22)
+print(add.__annotations__)
