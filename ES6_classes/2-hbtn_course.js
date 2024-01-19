b2c63996a54b3aa786bd96510c3f6e3dbb0473ca
Adib-96 <adibbh1996@gmail.com>
2024-01-19 18:18:43 +0100

    A Course, Getters, and Setters

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,35 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw TypeError('Name must be a string');
+    } else {
+      this._name = value;
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw TypeError('Length must be a number');
+    } else {
+      this._length = value;
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+}
