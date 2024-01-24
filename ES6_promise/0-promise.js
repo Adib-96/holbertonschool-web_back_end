be60a8df7632b86a6363e5bc1e96f5c12af680ec
Adib-96 <adibbh1996@gmail.com>
2024-01-24 09:19:31 +0100

    simple promise

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,4 @@
-function getResponseFromAPI() {
-    return new Promise((resolve,reject) => {
-    })
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+  });
 }
-
-const response = getResponseFromAPI();
-console.log(response instanceof Promise);
