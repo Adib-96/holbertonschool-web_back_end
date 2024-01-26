2d1431b8156a1b484edbbec9cf1cfadd1c5592df
Adib-96 <adibbh1996@gmail.com>
2024-01-26 17:25:34 +0100

    task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup () {
+export default function handleProfileSignup() {
   const result = Promise.all([uploadPhoto(), createUser()]);
   result.then((result) => {
     console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
-  });
+  })
+    .catch(() => console.log('Signup system offline'));
 
-  return result.catch(() => {
-    console.log('Signup system offline');
-  });
+  return result;
 }
