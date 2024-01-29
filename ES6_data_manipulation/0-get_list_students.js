a7d3f3e816addee7285a010e1993894bf38401d5
Adib-96 <adibbh1996@gmail.com>
2024-01-29 10:55:03 +0100

    task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,18 @@
+function getListStudents() {
+  const array = [];
+
+  array.push(
+    {
+      id: 1, firstname: 'Guillaume', location: 'San Francisco',
+    },
+    {
+      id: 2, firstname: 'James', location: 'Columbia',
+
+    },
+    {
+      id: 5, firstname: 'Serena', location: 'San Francisco',
+    },
+  );
+
+  return array;
+}
