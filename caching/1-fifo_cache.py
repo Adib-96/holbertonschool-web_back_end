72861b0a22a50f7ab385dd422c16be09ce695dbc
Adib-96 <adibbh1996@gmail.com>
2024-05-27 23:42:03 +0100

    FIFO caching

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -6,12 +6,19 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    ''' A basic cache.
+        Inherits from class BaseCaching.
+        Attributes:
+          put - method that adds a key/value pair to cache
+          get - method that retrieves a key/value pair from cache
+    '''
+
     def __init__(self):
         super().__init__()
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
+        ''' Add key/value pair to cache.
+        If either `key` or `item` is None, do nothing. '''
         if (key is None or item is None):
             return
         if len(self.cache_data) >= super().MAX_ITEMS:
@@ -23,8 +30,8 @@ class FIFOCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
-        """
+        ''' Return value stored in `key` of cache.
+        If key is None or does not exist in cache, return None. '''
         if key is None or key not in self.cache_data:
             return None
         else:
