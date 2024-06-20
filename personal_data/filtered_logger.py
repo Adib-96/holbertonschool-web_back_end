ea94bdf7b474a564157ae09a226a7f3c2f20ee9e
Adib-96 <adibbh1996@gmail.com>
2024-06-20 18:34:35 +0100

    Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,14 +1,14 @@
+#!/usr/bin/env python3
+"""
+python module that use Regex pattern
+
+"""
 import re
-fields = ["password", "date_of_birth"]
-messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-            "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
 
 
 def filter_datum(fields, redaction, message, separator):
     pattern = '|'.join(f'{field}=[^{separator}]*' for field in fields)
-    return re.sub(pattern, lambda m: f"{m.group(0).split('=')[0]}={redaction}", message)
-
-
-
-for message in messages:
-    print(filter_datum(fields, 'xxx', message, ';'))
+    return re.sub(
+        pattern,
+        lambda m: f"{m.group(0).split('=')[0]}={redaction}",
+        message)
