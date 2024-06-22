4e5e9e24671388031a9f3900fbd9b298e324c800
Adib-96 <adibbh1996@gmail.com>
2024-06-22 15:14:50 +0100

    add type annotated

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """
 0. Regex-ing: filter_datum
+1. Log formatter: class RedactingFormatter
 """
 import re
 import logging
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    """ Description: Regex-ing - Write a function called filter_datum that
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    ''' Description: Regex-ing - Write a function called filter_datum that
                      returns the log message obfuscated:
 
         Arguments:
@@ -21,7 +24,7 @@ def filter_datum(fields, redaction, message, separator):
         field values.
         filter_datum should be less than 5 lines long and use re.sub to
         perform the substitution with a single regex.
-    """
+    '''
 
     for i in fields:
         message = re.sub(i + "=.*?" + separator,
@@ -32,21 +35,27 @@ def filter_datum(fields, redaction, message, separator):
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+        Description: Update the class to accept a list of strings fields
+                     constructor argument.
+
+        Implement the format method to filter values in incoming log records
+        using filter_datum. Values for fields in fields should be filtered.
+
+        DO NOT extrapolate FORMAT manually. The format method should be less
+        than 5 lines long
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
+        """ Constructor Method """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        return filter_datum(
-            self.fields,
-            self.REDACTION,
-            super(
-                RedactingFormatter,
-                self).format(record),
-            self.SEPARATOR)
+        """ Filters values in incoming log records using filter_datum """
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
