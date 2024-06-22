0e4237099e3c51a51e3519fd32504dcd74f12da1
Adib-96 <adibbh1996@gmail.com>
2024-06-22 14:29:46 +0100

    implement a new regex logic

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,22 +7,23 @@ import re
 
 def filter_datum(fields, redaction, message, separator):
     ''' Description: Regex-ing - Write a function called filter_datum that
-                         returns the log message obfuscated:
+                     returns the log message obfuscated:
 
-            Arguments:
-                fields: a list of strings representing all fields to obfuscate
-                redaction: a string representing by what the field will be
-                           obfuscated
-                message: a string representing the log line
-                separator: a string representing by which character is
-                               separating all fields in the log line (message)
-            The function should use a regex to replace occurrences of certain
-            field values.
-            filter_datum should be less than 5 lines long and use re.sub to
-            perform the substitution with a single regex.
-        '''
-    pattern = '|'.join(f'{field}=[^{separator}]*' for field in fields)
-    return re.sub(
-        pattern,
-        lambda m: f"{m.group(0).split('=')[0]}={redaction}",
-        message)
+        Arguments:
+            fields: a list of strings representing all fields to obfuscate
+            redaction: a string representing by what the field will be
+                       obfuscated
+            message: a string representing the log line
+            separator: a string representing by which character is
+                           separating all fields in the log line (message)
+        The function should use a regex to replace occurrences of certain
+        field values.
+        filter_datum should be less than 5 lines long and use re.sub to
+        perform the substitution with a single regex.
+    '''
+
+    for i in fields:
+        message = re.sub(i + "=.*?" + separator,
+                         i + "=" + redaction + separator,
+                         message)
+    return message
