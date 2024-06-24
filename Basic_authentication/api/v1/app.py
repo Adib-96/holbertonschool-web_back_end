6b82b4d7baa5234939eb492a81bbf16ca4a3b561
Adib-96 <adibbh1996@gmail.com>
2024-06-24 15:43:55 +0100

    fix bug

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -25,7 +25,7 @@ def not_found(error) -> tuple:
 def unauthorized(error) -> tuple:
     """ unauthorized handler
     """
-    return jsonify({"error": "Unauthorized"}),401
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -26,6 +26,10 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized',strict_slashes=False)
-def unauthorized():
-    abort(401,description='Unauthorized')
+@app_views.route('/unauthorized', methodes=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+       Return:
+         - raises a 401 error by using abort
+       """
+    abort(401)
