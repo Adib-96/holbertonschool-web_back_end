a3c4dbca8ba52144e6ec2a85a546167ceae2b6c1
Adib-96 <adibbh1996@gmail.com>
2024-06-26 22:23:11 +0100

    Basic - Overload current_user - and BOOM!

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -77,6 +77,7 @@ class BasicAuth(Auth):
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ overloads Auth and retrieves the User instance for a request """
         auth_header = self.authorization_header(request)
 
         if not auth_header:
