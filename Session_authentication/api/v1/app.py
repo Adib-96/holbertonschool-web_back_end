7ad8016c10728ebd835a09332ed79e0a64005ef2
Adib-96 <adibbh1996@gmail.com>
2024-06-27 17:33:46 +0100

    before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -35,9 +35,9 @@ def before_request():
             if (auth.authorization_header(request) is None and
                     auth.session_cookie(request) is None):
                 abort(401)
-            if auth.current_user(request) is None:
-                abort(403)
-            request.current_user = auth.current_user(request)
+        if auth.current_user(request) is None:
+            abort(403)
+        request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(401)
