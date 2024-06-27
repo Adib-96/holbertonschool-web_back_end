375ebf4827d02d1b5c8a52dde7b327aba9a57f64
Adib-96 <adibbh1996@gmail.com>
2024-06-27 16:45:26 +0100

    me

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -20,20 +20,31 @@ if os.getenv("AUTH_TYPE") == "auth":
 
 
 @app.before_request
-def before_request():
-    """ Filtering of each request
+def before_request() -> str:
+    """ Before Request Handler
+    Requests Validation
     """
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
     if auth is None:
         return
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
+
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
-    if auth.current_user(request) is None:
+
+    # if auth.current_user(request) is None:
+    current_user = auth.current_user(request)
+    if current_user is None:
         abort(403)
-    request.current_user = auth.current_user(request)
+
+    request.current_user = current_user
 
 
 @app.errorhandler(401)
