3d03366b6347902695ca49d0e49101c6f6340d37
Adib-96 <adibbh1996@gmail.com>
2024-06-27 17:16:42 +0100

    Create a session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,18 @@
 """
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
+from typing import List, TypeVar
 
 
 class SessionAuth(Auth):
     """Session management class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''self descriptive'''
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
