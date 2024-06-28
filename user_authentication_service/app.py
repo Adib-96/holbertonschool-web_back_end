6a9940f136dd501c2082884dc3ba587801ef6582
Adib-96 <adibbh1996@gmail.com>
2024-06-28 18:29:50 +0100

    Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,14 +2,37 @@
 '''Flask application
 '''
 from flask import Flask, jsonify
+from auth import Auth
+from flask import request
+from flask import abort
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
 def index():
     """Root application"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
+    return jsonify(msg)
 
 
 if __name__ == "__main__":
