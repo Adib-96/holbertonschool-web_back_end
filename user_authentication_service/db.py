c61275da419b6dc78a70e7d744bcc06d6b64607c
Adib-96 <adibbh1996@gmail.com>
2024-06-28 02:04:10 +0100

    Find user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -34,14 +34,7 @@ class DB:
         return self.__session
 
     def add_user(self, email, hashed_password) -> User:
-        """_summary_ : add user to db
-
-        Args:
-            email (_type_): _description_
-            hashed_password (bool): _description_
-
-        Returns:
-            User: _description_
+        """summary : add user to db
         """
         new_user = User(email=email, hashed_password=hashed_password)
         seesion = self._session
