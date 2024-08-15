e0828bd57e90f4f98825f73fb045568c562a10a7
Adib-96 <adibbh1996@gmail.com>
2024-08-15 22:56:22 +0100

    list with Old school band

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,4 @@
--- script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name , COALESCE(split,2024) - formed AS lifespan FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+-- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%' ;
