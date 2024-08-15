3e7aa405f73ad04aa0f82997c1986789cbfd8ac6
Adib-96 <adibbh1996@gmail.com>
2024-08-15 22:18:52 +0100

    add colomun to the users table

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,4 @@
 -- This script creates a 'users' table in the database.
--- The table includes columns for user ID, email, name, and country.
--- The script will not fail if the table already exists due to the IF NOT EXISTS clause.
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
